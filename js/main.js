6d3a8938cee8d33ede1690c72f26f1e00f9571a7
Fabien Massucco <f9bm9s@gmail.com>
2020-08-18 11:57:45 +0200

    refacto : code design improvement & little fixes
    css coming soon!

--- js/main.js
+++ js/main.js
@@ -1,52 +1,61 @@
 const table = document.getElementById("countriesTable");
-const url =
+const urlApi =
   "https://restcountries.eu/rest/v2/region/europe?fields=name;capital;population;area;flag;borders;alpha3Code";
 
-fetch(url)
-  .then((resp) => resp.json())
-  .then((jsonResp) => tableize(jsonResp))
-  .catch((error) => console.log("Erreur : " + error));
+document.onload = loadCountries(urlApi);
+
+function loadCountries(url) {
+  fetch(url)
+    .then((resp) => resp.json())
+    .then((jsonResp) => tableize(jsonResp))
+    .catch((error) => console.log("Erreur : " + error));
+}
 
 function tableize(countries) {
-  return countries.map(function (country) {
-    let tr = createNode("tr"),
-      td1 = createNode("td"),
-      td2 = createNode("td"),
-      td3 = createNode("td"),
-      td4 = createNode("td"),
-      td5 = createNode("td"),
-      td6 = createNode("td"),
-      td7 = createNode("td");
-
-    td1.innerHTML = notNullCheck(country.name);
-    td2.innerHTML = notNullCheck(country.capital);
-    td3.innerHTML = notNullCheck(numformat(country.population));
-    td4.innerHTML = notNullCheck(numformat(country.area));
-    td5.innerHTML = densityCalc(country.population, country.area);
-    td6.innerHTML = notNullCheck(country.borders.length);
-    td7.innerHTML = notNullCheck(country.flag)
+  countries.forEach((country) => {
+    const flagIcon = notNullCheck(country.flag)
       ? '<img id="' + country.alpha3Code + 'FlagId" src="' + country.flag + '">'
       : "";
+    const cellVal = new Array(
+      notNullCheck(country.name),
+      notNullCheck(country.capital),
+      notNullCheck(numformat(country.population)),
+      notNullCheck(numformat(country.area)),
+      densityCalc(country.population, country.area),
+      notNullCheck(country.borders.length),
+      flagIcon
+    );
+    const tr = createNode("tr");
 
-    append(tr, td1);
-    append(tr, td2);
-    append(tr, td3);
-    append(tr, td4);
-    append(tr, td5);
-    append(tr, td6);
-    append(tr, td7);
+
+    for (let i = 0; i < cellVal.length; i++) {
+      const el = cellVal[i];
+      const td = createNode("td");
+      td.innerHTML = el;
+      append(tr, td);
+    }
+
     append(table, tr);
 
-    document
-      .getElementById(country.alpha3Code + "FlagId")
-      .addEventListener("click", function () {
-        document.getElementById("localMap").src =
-          "https://www.google.com/maps/embed/v1/place?key=AIzaSyAav3RZzTVK1b3FpvMaDKEI5CNzz-vdHlc&q=" +
-          country.capital +
-          "," +
-          country.name;
-      });
+    makeFlagClickable(country.alpha3Code, country.name, country.capital);
   });
+}
+
+function makeFlagClickable(alpha3Code, name, capital) {
+  document
+    .getElementById(alpha3Code + "FlagId")
+    .addEventListener("click", function () {
+      document
+        .getElementById("localMap")
+        .setAttribute(
+          "src",
+          "https://www.google.com/maps/embed/v1/place?key=AIzaSyAav3RZzTVK1b3FpvMaDKEI5CNzz-vdHlc&q=" +
+            capital +
+            "," +
+            name
+        );
+      window.location = "#map";
+    });
 }
 
 function densityCalc(pop, area) {
@@ -62,7 +71,7 @@ function notNullCheck(el) {
 }
 
 function numformat(x) {
-  return new Intl.NumberFormat("en-GB").format(Number.parseFloat(x).toFixed(3));
+  return new Intl.NumberFormat("en-GB", { maximumFractionDigits: 3 }).format(x);
 }
 
 function createNode(el) {
